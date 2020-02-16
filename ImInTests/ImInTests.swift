9520052d49c51e46915cc18749445955450375d2
ajinkyawani-unh <60481083+ajinkyawani-unh@users.noreply.github.com>
2020-02-16 18:28:50 -0500

    Initial Commit

---
+++ ImInTests/ImInTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImInTests.swift
+//  ImInTests
+//
+//  Created by Ajinkya Wani on 2/16/20.
+//  Copyright © 2020 Ajinkya Wani. All rights reserved.
+//
+
+import XCTest
+@testable import ImIn
+
+class ImInTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
